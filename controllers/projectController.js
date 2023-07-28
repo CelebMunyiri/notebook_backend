c8cfb327cbb9dd0d89a78b14b3d479bdde0b1e21
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-28 11:48:53 +0300

    anhanced ctreating note succesfully

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,7 @@
 const {v4}=require('uuid')
-const mssql=require('mssql')
+const mssql=require('mssql');
+const { createNotesTable } = require('../Database/Tables/notesTable');
+const { sqlConfig } = require('../config/config');
 //const notes=[] for test using noDB
 
 const createNote=async(req,res)=>{
@@ -10,13 +12,32 @@ const createNote=async(req,res)=>{
         const {note_title,note_content}=req.body
 const newNote={id,note_title,note_content,creation_time}
 
-//notes.push(newNote)
+const pool=await mssql.connect(sqlConfig)
+    if(pool.connected){
+res.json({
+ const result= await pool.request()
+        .input('id',mssql.VarChar,id)
+        .input('note_title', mssql.VarChar,note_title)
+        .input('note_content',mssql.VarChar,note_content)
+        .input('creation_time',mssql.Time,creation_time)
+        
 
+        //.query(`INSERT INTO projectsTable (id,project_name,description,project_location,startdate,enddate) VALUES (${id},${note_title},${note_content},${creation_time})`)//notes.push(newNote)
+.execute('createProjectPROC')
+
+
+if(result.rowsAffected == 1){
 res.json({
     message:'Note Created succesfully',
     note:newNote
 })
-    } catch (error) {
+})
+}else{
+    return res.json({message:'creation had isue'})
+}    
+    }
+    
+    catch (error) {
   return res.json({error})      
     }
 }
